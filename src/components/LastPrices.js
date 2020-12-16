8a805e37046db5d11dad368c9fb866aa3efef767
Bogdan <bogdan.kharchykov@gmail.com>
2020-12-16 21:58:27 +0100

    bgifx

--- src/components/LastPrices.js
+++ src/components/LastPrices.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 class LastPrices extends Component {
 
     render() {
-        let dateStr = ''
-        let date = new Date(this.props.last_update * 1000);
+        //let dateStr = ''
+        //let date = new Date(this.props.last_update * 1000);
         //dateStr = date.getHours() + ':' + ('0' + date.getMinutes()).substr(-2) + ':' + ('0' + date.getSeconds()).substr(-2);
         let eth_price = ''
         let btc_price = ''
